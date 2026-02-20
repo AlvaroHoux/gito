e3d7a2c38343102842d70d6344f6402e401c0f5e
Alvaro Houx <alvarohoux@gmail.com>
2026-02-20 16:44:40 -0300

    refactor(term): Update log formatting with warning/error colors and success color, introduce new functions for each color
    - Replaced WARN_COLOR and ERROR_COLOR with WARNING_COLOR and ERROR_COLOR respectively, maintaining the same color codes.
    - Introduced new functions: Warning(data ...any) for warnings, Success(data ...any) for successful outputs, adhering to the Conventional Commits format.
    - Updated printContent function to accept an additional color parameter, allowing for different formatting options (warnings, errors, success).

--- internal/term/print.go
+++ internal/term/print.go
@@ -3,12 +3,13 @@ package term
 import "fmt"
 
 const (
-	WARN_COLOR string = "\x1b[33m"
-	ERROR_COLOR string = "\x1b[35m"
+	WARNING_COLOR string = "\x1b[33m"
+	ERROR_COLOR   string = "\x1b[35m"
+	SUCCESS_COLOR string = "\033[1;92m"
 )
 
-func Warn(data ...any) {
-	printContent(WARN_COLOR, data...)
+func Warning(data ...any) {
+	printContent(WARNING_COLOR, data...)
 }
 
 func Error(err error) {
@@ -19,6 +20,9 @@ func Log(data ...any) {
 	printContent("", data...)
 }
 
+func Success(data ...any) {
+	printContent(SUCCESS_COLOR, data...)
+}
 
 func printContent(color string, data ...any) {
 	fmt.Printf("%s🐙 Gito: ", color)
